a28782497637fb8f07aa5bbef3fd1dbc0e8efa70
YM <YM@YM-PC>
2019-10-10 19:28:05 +0700

    Running sesuai ketentuan

---
+++ PBO-ulang-10116434-Latihan27-Hurufbesardankecil/src/pbo/ulang/pkg10116434/latihan27/hurufbesardankecil/PBOUlang10116434Latihan27Hurufbesardankecil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116434.latihan27.hurufbesardankecil;
+
+/**
+ *
+ * @author YM
+ */
+public class PBOUlang10116434Latihan27Hurufbesardankecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String str = "aPA aKu dI MArs";
+       String str1 = str.toUpperCase();
+       String str2 = str.toLowerCase();
+       System.out.println();
+       System.out.println("str = " + str);
+       System.out.println();
+       System.out.println("str.toUpperCase() = " + str1);
+       System.out.println("str.toLowerCase() = " + str2);
+        
+    }
+    
+}
