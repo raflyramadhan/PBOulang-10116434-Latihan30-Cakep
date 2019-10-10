15ccac1e247297042f6d8588e9cd2c3216ba392f
YM <YM@YM-PC>
2019-10-10 19:22:44 +0700

    Tidak running

---
+++ PBO-ulang-10116434-Latihan25-Ejaannama/src/pbo/ulang/pkg10116434/latihan25/ejaannama/PBOUlang10116434Latihan25Ejaannama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116434.latihan25.ejaannama;
+
+/**
+ *
+ * @author YM
+ */
+public class PBOUlang10116434Latihan25Ejaannama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
