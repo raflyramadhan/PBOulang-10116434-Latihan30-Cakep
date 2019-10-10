f780bf6b1449fe14f01906fb327a27d3391385da
YM <YM@YM-PC>
2019-10-10 18:58:49 +0700

    Running sesuai ketentuan

---
+++ PBO-ulang-10116434-Latihan18-Kelipatan/src/pbo/ulang/pkg10116434/latihan18/kelipatan/PBOUlang10116434Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116434.latihan18.kelipatan;
+
+/**
+ *
+ * @author YM
+ */
+public class PBOUlang10116434Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       double i=3.5;
+        
+        do{
+            {
+                System.out.println(i);
+            }
+            i+=3.5;
+        } while (i<=38);
+}
+}
+        
+        
+    
+    
+
